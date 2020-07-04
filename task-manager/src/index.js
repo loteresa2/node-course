dbef59c666b7c66305afa7fe6c65f51067822703
Teresa Lo <loteresa2@gmail.com>
2020-07-04 12:19:37 -0400

    API endpoint for model task and user

---
+++ task-manager/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body)
+
+  user.save().then(() => {
+    res.send(user)
+  }).catch(() => {
+    res.status(400).send(e)
+  })
+})
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body)
+
+  task.save().then(() => {
+    res.send(task)
+  }).catch(() => {
+    res.status(400).send(e)
+  })
+})
+
+app.listen(port, () => {
+  console.log("Server is up and running " + port)
+})
