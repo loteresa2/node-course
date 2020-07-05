8ec770559d6ad8ee1537e2a0e1891ffe0d664803
Teresa Lo <loteresa2@gmail.com>
2020-07-05 14:46:12 -0400

    initial setup for test

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,16 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+const app = require('./app')
+const port = port.env.PORT 
 
 app.listen(port, () => {
   console.log("Server is up and running " + port)
